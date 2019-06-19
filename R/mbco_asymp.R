86e00d06ddcac7b6a0d7e73e8350b026adef387b
Davood Tofighi <dtofighi@gmail.com>
2019-06-19 10:52:43 -0600

    Removed reject.

--- R/mbco_asymp.R
+++ R/mbco_asymp.R
@@ -6,14 +6,11 @@ mbco_asymp <- function(h0 = NULL,
   mbco_chisq <- mbco$diffLL[2]    # Chi-square
   mbco_pvalue <- mbco$p[2]      # p-value
   mbco_df <- mbco$df[2]-mbco$df[1]
-  mbco_reject <-
-    ifelse(mbco_pvalue < alpha, 1, 0) # reject or not
   mbcoTest <-
     list(
       chisq = mbco_chisq,
       df = mbco_df,
       p = mbco_pvalue,
-      reject = as.integer(mbco_reject)
     )
   return(mbcoTest)
 }
